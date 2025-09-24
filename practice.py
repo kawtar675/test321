c02a500d97d7a7464e377b5a7dcf9fe7e6bd04b1
kawtar675 <ke2712@g.rit.edu>
2025-09-24 23:17:21 +0400

    hello

---
+++ practice.py
@@ -0,0 +1,11 @@
+def add(num1,num2):
+    return x + y
+    
+def main():
+    
+    number1 = int(input("enter your first number: "))
+    number2 = int(input("enter your seccond number: "))
+    
+    result = add(number1, number2)
+    print("The sum is:", result)
+main()
